97553092c6ae80776598e9ddaaa5c4709d8085ae
Kennmle <kennmle@gmail.com>
2018-02-24 22:30:56 -0500

    Primative UI (More!)
    Now have basic functionality working. Current set-up is playing with 1-8
    on keyboard

--- New Unity Project/Assets/Scripts/Chord.cs
+++ New Unity Project/Assets/Scripts/Chord.cs
@@ -6,28 +6,25 @@ public class Chord{
 
 	AudioSource[] baseAudio;
 
-	bool isPlaying;
+	//bool isPlaying;
 
 	public Chord(AudioSource a, AudioSource b, AudioSource c) {
 		baseAudio = new AudioSource[3];
 		baseAudio[0] = a;
 		baseAudio[1] = b;
 		baseAudio[2] = c;
-		isPlaying = false;
+		//isPlaying = false;
 	}
 
 	public void play() {
-		if(!isPlaying) {
-			isPlaying = true;
 			foreach (AudioSource x in baseAudio) {
 				x.Stop();
 				x.Play();
 			}
-		}
 	}
 
 	public void stop() {
-		isPlaying=false;
+		//isPlaying=false;
 		foreach (AudioSource x in baseAudio)
 			x.Stop();
 	}
--- New Unity Project/Assets/Scripts/PlayerInput.cs
+++ New Unity Project/Assets/Scripts/PlayerInput.cs
@@ -1,86 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour {
-	public int threshold;
-	public Material mat;
+
 
 	private Chord[] chords;
 	private AudioSource[] notes;
+	private int[] hotkeyMapping;
 	private int transpose = 0;
 
-	private BitString	keys;
-	private int numOfKeys=20;
-	private int numOfChords=24;
+	private bool[] keys;
+	private int numOfKeys=8; //Number of keys to press
+	private int numOfTones=20; //Number of semitones to calculate
+	private int numOfChords=24; //Number of unique chords
+
+	// Currently has to specifically set the dropdowns as values in this array
+	// To automate later
+	public Dropdown[] profile;
 
 	// Use this for initialization
 	void Start () {
-		keys = new BitString(numOfKeys);
+		keys = new bool[numOfKeys];
 		chords = new Chord[numOfChords];
-		notes = new AudioSource[numOfKeys];
-		for(int i = 0; i < numOfKeys; ++i) {
-			AudioSource temp = GameObject.Instantiate(this.transform.GetChild(0).transform.GetChild(0), this.transform.GetChild(0)).GetComponent<AudioSource>();
+		notes = new AudioSource[numOfTones];
+		hotkeyMapping = new int[numOfKeys];
+		for(int i = 0; i < numOfTones; ++i) {
+			AudioSource temp = GameObject.Instantiate(this.transform.GetChild(0), this.transform).GetComponent<AudioSource>();
 			temp.pitch = Mathf.Pow(2, (i+transpose)/12.0f);
 			notes[i] = temp;
 		}
 
-		for(int i = 0; i < 12; ++i) {
+		//Sets bottom half of chords to major triads, upper half to minor
+		for(int i = 0; i < numOfChords/2; ++i) {
 			chords[i] = new Chord(notes[i], notes[i+4], notes[i+7]);
-			chords[12+i]= new Chord(notes[i], notes[i+3], notes[i+7]);
+			chords[numOfChords/2+i]= new Chord(notes[i], notes[i+3], notes[i+7]);
 		}
+		//Only for testing
 	}
 
 	// Update is called once per frame
 	void Update () {
 		changeKey();
+		updateHotbar(); //Currently inefficient
 		playChord();
 	}
 
+	// Takes input from 1-8 on keyboard
+	// Maybe add numpad option/remapping later
 	void changeKey() {
-		keys.set(0, Input.GetKey(KeyCode.Alpha1));
-		keys.set(1, Input.GetKey(KeyCode.Alpha2));
-		keys.set(2, Input.GetKey(KeyCode.Alpha3));
-		keys.set(3, Input.GetKey(KeyCode.Alpha4));
-		keys.set(4, Input.GetKey(KeyCode.Alpha5));
-		keys.set(5, Input.GetKey(KeyCode.Alpha6));
-		keys.set(6, Input.GetKey(KeyCode.Alpha7));
-		keys.set(7, Input.GetKey(KeyCode.Alpha8));
+		keys[0]= Input.GetKeyDown(KeyCode.Alpha1);
+		keys[1]= Input.GetKeyDown(KeyCode.Alpha2);
+		keys[2]= Input.GetKeyDown(KeyCode.Alpha3);
+		keys[3]= Input.GetKeyDown(KeyCode.Alpha4);
+		keys[4]= Input.GetKeyDown(KeyCode.Alpha5);
+		keys[5]= Input.GetKeyDown(KeyCode.Alpha6);
+		keys[6]= Input.GetKeyDown(KeyCode.Alpha7);
+		keys[7]= Input.GetKeyDown(KeyCode.Alpha8);
 	}
 
+	//Plays chords based on what's selected
 	void playChord() {
-		for(int i = 0; i < 8; ++i) {
-			if(keys.at(i)==1)
-				chords[i].play();
+		for(int i = 0; i < numOfKeys; ++i) {
+			if(keys[i])
+				chords[hotkeyMapping[i]].play();
+				/*
 			else if (keys.at(i)==0)
 				chords[i].stop(); //Note.stop does not actually stop the audio
+				*/
+
 		}
-		/*
-		//Naive implementation
-		if(keys==new BitString("100")) {
-			mat.color = Color.red;
+	}
+
+	//Goes through the dropdowns and updates values. Efficiency is currently questionable
+	void updateHotbar() {
+		for(int i = 0; i < numOfKeys; ++i) {
+			hotkeyMapping[i]=profile[i].value;
 		}
-		else if(keys==new BitString("110")) {
-			mat.color = Color.magenta;
-		}
-		else if(keys==new BitString("010")) {
-			mat.color = Color.blue;
-		}
-		else if(keys==new BitString("011")) {
-			mat.color = Color.cyan;
-		}
-		else if(keys==new BitString("001")) {
-			mat.color = Color.green;
-		}
-		else if (keys==new BitString("101")) {
-			mat.color = Color.yellow;
-		}
-		else if (keys==new BitString("111")) {
-			mat.color = Color.black;
-		}
-		else {
-			mat.color = Color.white;
-		}
-		*/
 	}
 }
