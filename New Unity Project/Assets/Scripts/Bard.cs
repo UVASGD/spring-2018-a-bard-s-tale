26408eb9902ee79db7b5d94d194490e398eae58d
Kennmle <kennmle@gmail.com>
2018-03-30 16:34:08 -0400

    Added Bard Class (2)
    Still being developed :(

---
+++ New Unity Project/Assets/Scripts/Bard.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Bard : MonoBehaviour {
+
+	// Use this for initialization
+	private Chord[] chordArr;
+	private int[] hotkeys;
+	private int prevChord;
+	private float timeDiff;
+	//private Ally[] allyArr;
+	private float[] musicQualities;
+	/*
+	0 = tempo
+	1 = freq
+	2 = vol
+	3 = tension
+	4 = tone
+	*/
+	private int[,] tensionArr;
+	private int[] toneArr;
+
+	int hotkeySize = 8;
+	int numOfChords = 24;
+	int numOfAllies = 0;
+
+	public AudioClip[] notes; //Temp solution
+
+	Event e;
+
+	void Start () {
+			/***** initialization of fields *****/
+			chordArr = new Chord[numOfChords];
+			hotkeys = new int[hotkeySize];
+			prevChord = 0;
+			timeDiff = 0;
+			//allyArr = Ally[n]; Need ally class first
+			musicQualities = new float[5];
+			//I'm putting these at the bottom since tension will be large.
+			setTension();
+			setTone();
+
+			/***** setting chords in chordArr *****/
+			//Need .wav files
+			//Might just do this manually in scene(?)
+			for(int i = 0; i<numOfChords; ++i) {
+				AudioSource x = gameObject.AddComponent<AudioSource>();;
+				x.clip = notes[i];
+				chordArr[i] = gameObject.AddComponent<Chord>();
+				chordArr[i].setAudio(x);
+			}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		e = Event.current;
+		if (e.type == EventType.KeyDown)
+		{
+				/** Handles all button presses! **/
+				int key = (int)e.keyCode - (int)KeyCode.Alpha1;
+				if (key >= 0 && key < hotkeySize)	{
+						playChord(key);
+						prevChord = hotkeys[key];
+				}
+		}
+
+		for(int i = 0; i < numOfChords; ++i) {
+			if(i != prevChord) {
+				chordArr[i].stop();
+			}
+		}
+		timeDiff += Time.deltaTime;
+	}
+
+	void playChord(int num) { //the number of the button pressed is passed in
+		int chord = hotkeys[num];
+		chordArr[chord].play();
+		//tensionArr[chord][prevChord]
+		//toneArr[chord]
+		prevChord = chord;
+		timeDiff = 0;
+	}
+
+	void affectAllies() {
+
+	}
+
+	void changeHotkeys(int[] newKeys) {
+		hotkeys = newKeys;
+	}
+
+	void changeVol(int v) {
+
+	}
+
+	void changeTempo(int t) {
+
+	}
+
+	void getTensionMod() {
+
+	}
+
+	void getToneMod() {
+
+	}
+
+	void setTension() {
+		tensionArr = new int[,] { {0, 0, 0, 0, 5, 4, 0, 0, 6, 1, 5, 1},
+									{-1, 0, 0, 0, 0, 0, 3, 0, 0, 0, -3, 3},
+									{-1, -3, 0, 1, 0, 0, 1, 0, 0, 0, 0, -2},
+									{-1, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 2},
+									{-2, 0, 0, -3, 0, 2, 0, 0, 1, 1, -2, 0},
+									{-5, 2, 0, 0, 1, 0, 0, 0, 4, 2, 3, 5},
+									{-1, -3, 2, 0, 0, 0, 0, 1, 0, 1, 2, 2},
+									{-2, 0, 1, 1, 0, 0, -2, 0, 0, 0, 0, 0},
+									{-3, 0, 0, -1, 0, -2, 0, 0, 0, -2, 4, 0},
+									{-2, 0, 0, 1, 3, 2, 2, 0, 3, 0, 4, 2},
+									{-7, -2, 0, 0, 1, -3, -4, 1, 1, -4, 0, 2},
+									{-3, -3, 2, 0, 0, 2, -3, 0, 1, -2, -1, 0}};
+	}
+
+	void setTone() {
+
+		toneArr = new int[] {3, -1, -2, -2, 1, 2, -2, -1, -2, 2, 2, 1, -2, -1, 2, 1, -2, -2, 2, -3, 1, -2, 1, 1};
+
+	}
+}
--- New Unity Project/Assets/Scripts/PlayerInput.cs
+++ New Unity Project/Assets/Scripts/PlayerInput.cs
@@ -7,13 +7,11 @@ public class PlayerInput : MonoBehaviour {
 
 
 	private Chord[] chords;
-	private AudioSource[] notes;
+	public AudioClip[] notes;
 	private int[] hotkeyMapping;
-	private int transpose = 0;
 
 	private bool[] keys;
 	private int numOfKeys=8; //Number of keys to press
-	private int numOfTones=20; //Number of semitones to calculate
 	private int numOfChords=24; //Number of unique chords
 
 	// Currently has to specifically set the dropdowns as values in this array
@@ -24,20 +22,27 @@ public class PlayerInput : MonoBehaviour {
 	void Start () {
 		keys = new bool[numOfKeys];
 		chords = new Chord[numOfChords];
-		notes = new AudioSource[numOfTones];
+		//notes = new AudioSource[numOfTones];
 		hotkeyMapping = new int[numOfKeys];
-		for(int i = 0; i < numOfTones; ++i) {
-			AudioSource temp = GameObject.Instantiate(this.transform.GetChild(0), this.transform).GetComponent<AudioSource>();
-			temp.pitch = Mathf.Pow(2, (i+transpose)/12.0f);
-			notes[i] = temp;
-			loadChords();
-		}
 
 		//Sets bottom half of chords to major triads, upper half to minor
-		for(int i = 0; i < numOfChords/2; ++i) {
-			chords[i] = new Chord(notes[i], notes[i+4], notes[i+7]);
-			chords[numOfChords/2+i]= new Chord(notes[i], notes[i+3], notes[i+7]);
+		/*
+		for(int i = 0; i < numOfChords/2; i++) {
+			chords[2*i] = new Chord(notes[i]);
+			chords[2*i+1] = new Chord(notes[i]);
 		}
+		*/
+
+		for(int i = 0; i<numOfChords; ++i) {
+			AudioSource x = gameObject.AddComponent<AudioSource>();;
+			x.clip = notes[i];
+			chords[i] = gameObject.AddComponent<Chord>();
+			chords[i].setAudio(x);
+		}
+
+
+
+		loadChords();
 		//Only for testing
 	}
 
@@ -46,20 +51,33 @@ public class PlayerInput : MonoBehaviour {
 		changeKey();
 		updateHotbar(); //Currently inefficient
 		saveChords();
-		playChord();
 	}
 
 	// Takes input from 1-8 on keyboard
 	// Maybe add numpad option/remapping later
 	void changeKey() {
-		keys[0]= Input.GetKeyDown(KeyCode.Alpha1);
-		keys[1]= Input.GetKeyDown(KeyCode.Alpha2);
-		keys[2]= Input.GetKeyDown(KeyCode.Alpha3);
-		keys[3]= Input.GetKeyDown(KeyCode.Alpha4);
-		keys[4]= Input.GetKeyDown(KeyCode.Alpha5);
-		keys[5]= Input.GetKeyDown(KeyCode.Alpha6);
-		keys[6]= Input.GetKeyDown(KeyCode.Alpha7);
-		keys[7]= Input.GetKeyDown(KeyCode.Alpha8);
+		bool[] keys1 = new bool[numOfKeys];
+		keys1[0]= Input.GetKeyDown(KeyCode.Alpha1);
+		keys1[1]= Input.GetKeyDown(KeyCode.Alpha2);
+		keys1[2]= Input.GetKeyDown(KeyCode.Alpha3);
+		keys1[3]= Input.GetKeyDown(KeyCode.Alpha4);
+		keys1[4]= Input.GetKeyDown(KeyCode.Alpha5);
+		keys1[5]= Input.GetKeyDown(KeyCode.Alpha6);
+		keys1[6]= Input.GetKeyDown(KeyCode.Alpha7);
+		keys1[7]= Input.GetKeyDown(KeyCode.Alpha8);
+
+		bool changed = false;
+		for(int i = 0; i<numOfKeys; i++) {
+			if(keys1[i]) {
+				changed = true;
+				break;
+			}
+		}
+
+		if(changed) {
+			keys=keys1;
+			playChord();
+		}
 	}
 
 	//Plays chords based on what's selected
@@ -67,11 +85,8 @@ public class PlayerInput : MonoBehaviour {
 		for(int i = 0; i < numOfKeys; ++i) {
 			if(keys[i])
 				chords[hotkeyMapping[i]].play();
-				/*
-			else if (keys.at(i)==0)
-				chords[i].stop(); //Note.stop does not actually stop the audio
-				*/
-
+			else
+				chords[hotkeyMapping[i]].stop(); //Note.stop does not actually stop the audio
 		}
 	}
 
