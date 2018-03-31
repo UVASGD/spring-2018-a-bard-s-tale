05a4315e4fb04fc46269081fa35dc93ef50634f8
Cycerio <nhc6yy@virginia.edu>
2018-03-30 20:43:04 -0400

    Toggle Changes with Scene

---
+++ BardsTale/Assets/Scripts/TempoChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempoChange : MonoBehaviour {
+	private Slider tempoSlider;
+	public TempoToggle tempo;
+
+	// Use this for initialization
+	void Start () {
+		tempo = GameObject.FindGameObjectWithTag ("ToggleT").GetComponent<> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
