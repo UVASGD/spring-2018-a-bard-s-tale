1ef0f8ce4a9edb5a6b84a7c10130fda918554f63
Kennmle <kennmle@gmail.com>
2018-04-21 13:20:15 -0400

    UI Scene
    Demo UI Scene added to the project. A through H for notes (they don't
    play), W and E to change the bar at the top, and Z, X, and C to change
    the number of hearts

---
+++ BardsTale/Assets/CameraUI/MoveCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate(new Vector3(-1f*speed,0f,0f)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(new Vector3(1f*speed,0f,0f)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate(new Vector3(0f,1f*speed,0f)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			transform.Translate(new Vector3(0f,-1f*speed,0f)*Time.deltaTime);
+		}
+	}
+}
