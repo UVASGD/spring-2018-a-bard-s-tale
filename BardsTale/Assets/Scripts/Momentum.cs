9db72bcac6de4537986de4525609b6c7f79bcdcc
Kenny Chen <kc5by@virginia.edu>
2018-04-28 15:03:58 -0400

    Basic Battle Scene

--- BardsTale/Assets/Scripts/Momentum.cs
+++ BardsTale/Assets/Scripts/Momentum.cs
@@ -1,48 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 using UnityEngine.UI;
 
 public class Momentum : MonoBehaviour
 {
     public Slider momentumSlider;
 
-    public GameObject person;
-    public GameObject[] allies;
-    public GameObject[] enemies;
-
-    private float playerHealth;
-    private float[] allyHealths;
-    private float[] enemyHealths;
-
-    void Start()
+    //function to change momentum
+    void changeMomentum(float dmg)
     {
-        playerHealth = 100;
-
-        allyHealths = new float[allies.Length];
-        for(int i = 0; i < allies.Length; i++)
-        {
-
-        }
+        momentumSlider.value += dmg * Time.deltaTime;
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        momentumSlider.value += Input.GetAxis("Horizontal") * Time.deltaTime;
-=======
-
-public class Momentum : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
->>>>>>> origin/master
-	}
 }
